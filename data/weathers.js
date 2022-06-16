1a2f094af897703c7099a407ec8730af78af40e5
jake-aro <103160476+jake-aro@users.noreply.github.com>
2022-06-16 17:43:02 +1200

    data

---
+++ data/weathers.js
@@ -0,0 +1,91 @@
+export default {
+  HawkesBay: [
+    {
+      day: 'Wednesday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+
+    {
+      day: 'Thursday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+
+    {
+      day: 'Saturday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+
+    {
+      day: 'Sunday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+  ],
+
+  Wellington: [
+    {
+      day: 'Wednesday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+
+    {
+      day: 'Thursday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+
+    {
+      day: 'Saturday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+
+    {
+      day: 'Sunday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+  ],
+
+  Queenstown: [
+    {
+      day: 'Wednesday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+
+    {
+      day: 'Thursday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+
+    {
+      day: 'Saturday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+
+    {
+      day: 'Sunday',
+      temperature: '37C',
+      description: 'Sunshine all day!',
+      emoji: '👨‍🎤',
+    },
+  ],
+}
