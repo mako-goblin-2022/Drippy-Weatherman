a1dfeb1185a683b1e3864e18330ec0d7d8f44936
Yuri Cowan <Yuri@Cecilias-MBP.fritz.box>
2022-06-16 17:54:12 +1200

    components added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import Navbar from './Navbar'
+import Weather from './Weather'
 
 const App = () => {
   return (
     <>
-      <h1>Drippy</h1>
+      <Navbar />
+      <Weather /> 
+      <h1>App display here</h1>
     </>
   )
 }
---
+++ client/components/Weather.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Weather = () => {
+  return <p>Weather input here</p>
+}
+
+export default Weather
