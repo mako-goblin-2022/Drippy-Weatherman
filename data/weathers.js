799d1ea6ed5fc1b2decac1bd4516b8d5a514d798
yuricowan <103348152+yuricowan@users.noreply.github.com>
2022-06-17 15:13:38 +1200

    update weather.js data

--- data/weathers.js
+++ data/weathers.js
@@ -1,29 +1,28 @@
 export default {
   Auckland: [
     {
-      day: 'Wednesday',
+      day: 'Saturday',
       temperature: '23',
-      description:
-        'rain, fine, rain, fine, rain, fine. The usual.',
+      description: 'rain, fine, rain, fine, rain, fine. The usual.',
       emoji: '🌞 ☔️🌞 ☔️🌞 ☔️ ',
     },
 
     {
-      day: 'Thursday',
+      day: 'Sunday',
       temperature: '3',
       description: '4-seasons in one day',
       emoji: '❄️🌞☔️🌈⚡️',
     },
 
     {
-      day: 'Saturday',
+      day: 'Monday',
       temperature: '26',
       description: `Oh boy you're in for a humid one. Stay hydrated folks`,
       emoji: '🌦️🫠🚰',
     },
 
     {
-      day: 'Sunday',
+      day: 'Tuesday',
       temperature: '6',
       description: 'Cloudy. Good day to mow the lawns',
       emoji: '🌦️🐝',
@@ -32,28 +31,28 @@ export default {
 
   Wellington: [
     {
-      day: 'Wednesday',
+      day: 'Saturday',
       temperature: '11',
       description: 'Windy',
       emoji: '🌬🌊😕',
     },
 
     {
-      day: 'Thursday',
+      day: 'Sunday',
       temperature: '15',
       description: 'Windy and rainy',
       emoji: '🌪💦😩',
     },
 
     {
-      day: 'Saturday',
+      day: 'Monday',
       temperature: '12',
       description: `Basically a tornado, pouring with rain`,
       emoji: '🫣🌪⛈🌪⛈🌪⛈🌪⛈😵',
     },
 
     {
-      day: 'Sunday',
+      day: 'Tuesday',
       temperature: '12',
       description: `YOU HAVE ONE DAY OF GOOD WEATHER, GET OUTSIDE`,
       emoji: '🌞🌻🌝',
@@ -62,28 +61,28 @@ export default {
 
   Queenstown: [
     {
-      day: 'Wednesday',
+      day: 'Saturday',
       temperature: '-4',
       description: `**SNOW ALERT** Crank the heatpump!`,
       emoji: '❄️⛄🥶',
     },
 
     {
-      day: 'Thursday',
+      day: 'Sunday',
       temperature: '-39',
       description: 'Horrible. Absolutely disgusting. Go see a movie',
       emoji: '⛈🏡🎞',
     },
 
     {
-      day: 'Saturday',
+      day: 'Monday',
       temperature: '-2',
       description: 'Stunner. Take the kids out for brunch',
       emoji: '🌞👨‍👩‍👧‍👧🥞',
     },
 
     {
-      day: 'Sunday',
+      day: 'Tuesday',
       temperature: '-5',
       description: 'Beauty! Hit the slopes!!',
       emoji: '⛄❄️🎿⛷🏂',
